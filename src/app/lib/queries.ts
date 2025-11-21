534712d37ab3aa1e0cc8ef2c47d007d036b39110
Diego Chaves <chavsdiego@gmail.com>
2025-11-21 20:51:28 -0300

    add more images

--- src/app/lib/queries.ts
+++ src/app/lib/queries.ts
@@ -22,11 +22,11 @@ const temp_images = [
   "https://images.unsplash.com/photo-1453847668862-487637052f8a",
   "https://images.unsplash.com/photo-1616391182219-e080b4d1043a",
   "https://images.unsplash.com/photo-1629909613654-28e377c37b09",
-  "https://plus.unsplash.com/premium_photo-1675686363477-c28d5bf65adb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1582798993202-b2d35d9620be",
-  "https://images.unsplash.com/photo-1550998592-38379c536449",
-  "https://images.unsplash.com/photo-1584281896894-3a5570d5885e",
-  "https://images.unsplash.com/photo-1505751172876-fa1923c58548",
+  "https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d",
+  "https://images.unsplash.com/photo-1631248055158-edec7a3c072b",
+  "https://images.unsplash.com/photo-1453847668862-487637052f8a",
+  "https://images.unsplash.com/photo-1616391182219-e080b4d1043a",
+  "https://images.unsplash.com/photo-1629909613654-28e377c37b09"
 ];
 
 export function getAllClinics() {
